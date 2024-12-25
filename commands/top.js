28b7cfc65cfdc89d68bf171e66cf7a4aaed39f1f
hr951 <123540607+hr951@users.noreply.github.com>
2024-12-25 19:02:56 +0900

    Create top.js

---
+++ commands/top.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder , EmbedBuilder } = require('discord.js');
+
+    module.exports = {
+      data: new SlashCommandBuilder()
+        .setName('top')
+        .setDescription('チャンネルの一番上のメッセージのリンクを送信します'),
+      async execute(interaction) {
+        const channel = interaction.channel;
+        const messages = await channel.messages.fetch({ after: '0', limit: 1 });
+        const message = messages.first();
+        const link = message.url;
+    
+        // リンクを返信する    
+        
+        const embed = new EmbedBuilder()
+  .setTitle("チャンネル最上部へ")
+  .setURL(link)
+  .setColor("#FFFFFF")
+     await interaction.reply({ embeds: [embed] , allowedMentions: { repliedUser: false } })
+      },
+    };
