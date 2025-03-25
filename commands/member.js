76c3b134be1b8b1d339fdf05bdf39713d934c296
hr951 <123540607+hr951@users.noreply.github.com>
2025-03-25 11:27:04 +0900

    Update member.js

--- commands/member.js
+++ commands/member.js
@@ -1,67 +1,80 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('member')
-		.setDescription('勢力ごとのメンバーを表示します'),
+    data: new SlashCommandBuilder()
+        .setName('member')
+        .setDescription('勢力ごとのメンバーを表示します'),
 
-	async execute(interaction) {
+    async execute(interaction) {
 
-		const thumbnail = interaction.client.user.displayAvatarURL();
+        const thumbnail = interaction.client.user.displayAvatarURL();
         const color = "#ffffff";
-        
+
         var member_1 = "なし";
         var member_2 = "なし";
         var member_3 = "なし";
+        var member_4 = "なし";
 
         const role_1 = await interaction.guild.roles.fetch('1265668863597740225');
-        if (role_1.members.size !== 0){
-        var member_1 = await role_1.members.map(m=>m.nickname || m.user.globalName).join('\n') ;
+        if (role_1.members.size !== 0) {
+            var member_1 = await role_1.members.map(m => m.nickname || m.user.globalName).join('\n');
         } else {
-        var member_1 = "なし";
+            var member_1 = "なし";
         };
 
         const role_2 = await interaction.guild.roles.fetch('1265668095427612703');
-        if (role_2.members.size !== 0){
-        var member_2 = await role_2.members.map(m=>m.nickname || m.user.globalName).join('\n');
+        if (role_2.members.size !== 0) {
+            var member_2 = await role_2.members.map(m => m.nickname || m.user.globalName).join('\n');
         } else {
-        var member_2 = "なし";
+            var member_2 = "なし";
         };
 
         const role_3 = await interaction.guild.roles.fetch('1268835638686257203');
-        if (role_3.members.size !== 0){
-        var member_3 = await role_3.members.map(m=>m.nickname || m.user.globalName).join('\n');
+        if (role_3.members.size !== 0) {
+            var member_3 = await role_3.members.map(m => m.nickname || m.user.globalName).join('\n');
         } else {
-        var member_3 = "なし";
+            var member_3 = "なし";
         };
 
-	    const embed = new EmbedBuilder()
+        const role_4 = await interaction.guild.roles.fetch('1353728683357114369');
+        if (role_4.members.size !== 0) {
+            var member_4 = await role_4.members.map(m => m.nickname || m.user.globalName).join('\n');
+        } else {
+            var member_4 = "なし";
+        };
+
+        const embed = new EmbedBuilder()
             .setTitle("勢力に加入しているメンバー")
             .addFields(
                 {
-                  name: `**${role_1.name}** (**${role_1.members.size}**人)`,
-                  value: member_1,
-                  inline: true
+                    name: `**${role_1.name}** (**${role_1.members.size}**人)`,
+                    value: member_1,
+                    inline: true
                 },
                 {
-                  name: `**${role_2.name}** (**${role_2.members.size}**人)`,
-                  value: member_2,
-                  inline: true
+                    name: `**${role_2.name}** (**${role_2.members.size}**人)`,
+                    value: member_2,
+                    inline: true
                 },
                 {
-                  name: `**${role_3.name}** (**${role_3.members.size}**人)`,
-                  value: member_3,
-                  inline: true
+                    name: `**${role_3.name}** (**${role_3.members.size}**人)`,
+                    value: member_3,
+                    inline: true
                 },
-              )
-			.setColor(color)
-      		.setFooter({
-        			text: "Made by Mina鯖 Bot",
-        			iconURL: thumbnail,
-      					})
-			.setTimestamp();
+                {
+                    name: `**${role_4.name}** (**${role_4.members.size}**人)`,
+                    value: member_4,
+                    inline: true
+                },
+            )
+            .setColor(color)
+            .setFooter({
+                text: "Made by Mina鯖 Bot",
+                iconURL: thumbnail,
+            })
+            .setTimestamp();
 
-			await interaction.reply({ embeds: [embed] })
+        await interaction.reply({ embeds: [embed] })
 
-	},
+    },
 };
