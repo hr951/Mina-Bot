dc95618d08b74483f052840b3345963184ee5fd3
hr951 <123540607+hr951@users.noreply.github.com>
2025-12-05 22:38:04 +0900

    Update msgSchema with additional fields
    Added new fields for background type, URL, and roles.

--- db/db.js
+++ db/db.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 
 const msgSchema = new mongoose.Schema({
-	_id: { type: String }, //ユーザーID
-	name: { type: String }, //ユーザーネーム
-	content: { type: String }, //メッセ
+    _id: { type: String }, //ユーザーID
+    name: { type: String }, //ユーザーネーム
+    content: { type: String }, //メッセ
     msgcount: { type: Number }, //メッセ数
-	msglength: { type: Number}, //メッセ総長さ
     point: { type: Number }, //所持ポイント
     all_point: { type: Number }, //累計ポイント
     bg_upgrade: { type: Boolean }, //背景変化
+    bg_type: { type: Number }, //0...デフォルト、1...既定アップグレード, 2...自由画像
+    bg_url: { type: String },
+    anni_role: { type: Boolean },
+    osyaberi_role: { type: Boolean },
+    densetu_role: { type: Boolean }
 });
 
 const model = mongoose.model('Messages', msgSchema);
