53237f54e8b2c824b2ab2a06865189663f55c9fe
hr951 <123540607+hr951@users.noreply.github.com>
2026-02-06 00:06:59 +0900

    音楽再生の不具合の一次修正

--- commands/play.js
+++ commands/play.js
@@ -43,46 +43,81 @@ module.exports = {
 
     if (!player.playing && !player.paused && queue.length === 1) {
       await playNext(player, kazagumo);
-      return interaction.editReply({ content: `**${query}** を追加しました\n※サーバーの稼働状況によって取得先が変わります`, ephemeral: true });
+      return await interaction.editReply({ content: `**${query}** を追加しました\n※サーバーの稼働状況によって取得先が変わります`, ephemeral: true });
     }
 
-    return interaction.editReply({ content: `**${query}** を追加しました\n※サーバーの稼働状況によって取得先が変わります`, ephemeral: true });
+    return await interaction.editReply({ content: `**${query}** を追加しました\n※サーバーの稼働状況によって取得先が変わります`, ephemeral: true });
   },
 };
 
 async function playNext(player, kazagumo) {
-
   const queue = global.customQueue.get(player.guildId);
   if (!queue || queue.length === 0) return;
 
-  const nextItem = queue[0]; // 先頭を取得
-  const query = nextItem.query;
+  const current = queue[0];
   const homeIp = process.env.HOME_API_URL;
-  const proxyUrl = `http://${homeIp}/stream?query=${encodeURIComponent(query)}`;
+  const proxyUrl = `http://${homeIp}/stream?query=${encodeURIComponent(current.query)}`;
 
   let track = null;
+  let metadata = null;
 
   try {
     const result = await kazagumo.search(proxyUrl, { engine: "http" });
-    if (result && result.tracks.length > 0) {
-      track = result.tracks[0];
-      const yt = await yts(query).catch(() => null);
-      if (yt && yt.videos[0]) {
-        track.title = yt.videos[0].title;
-        track.thumbnail = yt.videos[0].thumbnail;
-        track.author = yt.videos[0].author.name;
-        track.uri = yt.videos[0].url;
+    const infoRes = await fetch(`http://${homeIp}/info?query=${encodeURIComponent(current.query)}`);
+    if (infoRes.ok) {
+      metadata = await infoRes.json();
+
+      console.log(result)
+
+      if (await result.tracks.length > 0) {
+        track = result.tracks[0];
+        track.title = metadata.title;
+        track.author = metadata.author;
+        track.thumbnail = metadata.thumbnail;
+        track.uri = metadata.url;
+        track.length = metadata.duration;
         track.source = true;
       }
+
+      console.log(track)
     }
-  } catch {
-    console.log(`[Home Server] 接続失敗、SoundCloudへ切り替えます: ${query}`);
+  } catch (error) {
+    console.error("自宅サーバーからの情報取得に失敗:", error.message);
   }
 
   if (!track) {
-    // --- Render Lava ---
+    // --- SoundCloud Author込み取得Ver ---
     // YouTubeから情報を取得
-    const ytResult = await yts(query).catch(() => null);
+    const ytResult = await yts(current.query).catch(() => null);
+
+    if (!ytResult || !ytResult.videos.length) {
+      console.log("YouTube情報なし");
+      queue.shift();
+      playNext(player, kazagumo);
+    }
+
+    const video = ytResult.videos[0];
+    const ytDurationMs = video.duration.seconds * 1000;
+
+    // SoundCloudで検索
+    const searchTitle = `${video.title} ${video.author.name}`;
+    const res = await kazagumo.search(searchTitle, { engine: "soundcloud" });
+
+    if (!res.tracks.length) {
+      console.log("SoundCloud情報なし");
+      queue.shift();
+      playNext(player, kazagumo);
+      return;
+    }
+
+    // カバー回避用フィルタ（秒数誤差15秒以内）
+    track = res.tracks.find(t => Math.abs(t.length - ytDurationMs) < 15000) || res.tracks[0];
+  }
+
+  if (!track) {
+    // --- SoundCloud Authorなし取得Ver ---
+    // YouTubeから情報を取得
+    const ytResult = await yts(current.query).catch(() => null);
 
     if (!ytResult || !ytResult.videos.length) {
       console.log("YouTube情報なし");
