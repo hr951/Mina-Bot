91a439e39670a29e3f4db90210fc553d9d39d47b
hr951 <hiroki.saki.951@gmail.com>
2026-02-15 06:37:47 +0900

    復旧完了

--- events/clientReady.js
+++ events/clientReady.js
@@ -44,12 +44,12 @@ module.exports = {
         client.user.setPresence({
             activities: [
                 {
-                    //name: `Minachanの広場`,
-                    name: "エラー処理中……",
+                    name: `Minachanの広場`,
+                    //name: "エラー処理中……",
                     type: ActivityType.Competing
                 }
             ],
-            status: PresenceUpdateStatus.Idle // Online : いつもの, DoNotDisturb : 赤い奴, Idle : 月のやつ, Invisible : 表示なし
+            status: PresenceUpdateStatus.Online // Online : いつもの, DoNotDisturb : 赤い奴, Idle : 月のやつ, Invisible : 表示なし
         });
 
         console.log(`Logged in as ${client.user.tag}`);
