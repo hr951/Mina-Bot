6ff8005eceb59ac98767e2d3420a5c17b7c3b92b
hr951 <hiroki.saki.951@gmail.com>
2026-05-23 00:30:08 +0900

    ロール処理に削除するロールを追加

--- interactions/buttons/config_acceptrules.js
+++ interactions/buttons/config_acceptrules.js
@@ -7,9 +7,11 @@ module.exports = {
             try {
                 const role = await interaction.guild.roles.fetch("1356110722571964592");
                 const role_2 = await interaction.guild.roles.fetch("1507341062925062164");
+                const role_3 = await interaction.guild.roles.fetch("1507341579806048358");
                 const member = await interaction.member.fetch();
                 await member.roles.add(role);
                 await member.roles.add(role_2);
+                await member.roles.remove(role_3);
                 await interaction.reply({
                     content: "認証されました。\nご協力ありがとうございます。",
                     flags: [MessageFlags.Ephemeral]
