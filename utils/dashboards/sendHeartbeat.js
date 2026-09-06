1ab0278054c30076aa4c2e7db44900907ee4f6f3
hr951 <hiroki.saki.951@gmail.com>
2026-09-06 19:05:53 +0900

    コンソール処理とかいろいろ

--- utils/dashboards/sendHeartbeat.js
+++ utils/dashboards/sendHeartbeat.js
@@ -1,70 +1,61 @@
 require("dotenv").config();
 const { getCPUUsage, getRAMUsage, getDBUsage } = require("../getUsage");
+const { ms2time } = require("../ms2time");
 const { version } = require('discord.js');
 const os = require('os');
 
 async function sendHeartbeat(client) {
-  try {
-    console.log("[heartbeat] start");
 
-    const uptime = client.uptime;
-    const nodeVer = process.version;
-    const djsVer = version;
-    const osVer = os.type();
-    const ping = client.ws.ping;
-    console.log("[heartbeat] basic info ok", { uptime, ping });
+  const uptime = ms2time(client.uptime);
+  const nodeVer = process.version;
+  const djsVer = version;
+  const osVer = os.type();
+  const ping = client.ws.ping;
 
-    const cpuUsage = await getCPUUsage();
-    console.log("[heartbeat] cpuUsage ok", cpuUsage);
+  const cpuUsage = await getCPUUsage();
 
-    const ramUsage = await getRAMUsage();
-    console.log("[heartbeat] ramUsage ok", ramUsage);
+  const ramUsage = await getRAMUsage();
 
-    const dbUsage = await getDBUsage();
-    console.log("[heartbeat] dbUsage ok", dbUsage);
+  const dbUsage = await getDBUsage();
 
-    const res = await fetch("https://api.crystium.net/api/bot/report", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Bot-Secret": process.env.BOT_REPORT_SECRET,
+  await fetch("https://api.crystium.net/api/bot/report", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Bot-Secret": process.env.BOT_REPORT_SECRET,
+    },
+    body: JSON.stringify({
+      bot_id: client.user.id,
+      bot_name: client.user.username,
+      bot_avatar_url: client.user.displayAvatarURL({ format: "png", size: 512 }),
+      system: {
+        os: osVer,
+        uptime_seconds: uptime,
+        ping_ms: ping,
+        ram_usage_mb: ramUsage.botUsed,
+        cpu_usage_percent: cpuUsage,
+        db_usage_mb: dbUsage.used,
+        node_version: nodeVer,
+        discordjs_version: djsVer,
       },
-      body: JSON.stringify({
-        bot_id: client.user.id,
-        bot_name: client.user.username,
-        bot_avatar_url: client.user.displayAvatarURL({ format: "png", size: 512 }),
-        system: {
-          os: osVer,
-          uptime_seconds: uptime,
-          ping_ms: ping,
-          ram_usage_mb: ramUsage.botUsed,
-          cpu_usage_percent: cpuUsage,
-          db_usage_mb: dbUsage.used,
-          node_version: nodeVer,
-          discordjs_version: djsVer,
-        },
-        discord_stats: {
-          guild_count: client.guilds.cache.size,
-          total_users: client.guilds.cache.reduce((acc, g) => acc + g.memberCount, 0),
-          cached_users: client.users.cache.size,
-          channels_count: client.channels.cache.size,
-        },
-        guilds: client.guilds.cache.map((g) => ({
-          id: g.id,
-          name: g.name,
-          icon_url: g.iconURL(),
-          member_count: g.memberCount,
-          channel_count: g.channels.cache.size,
-          boost_level: g.premiumTier,
-        })),
-        last_heartbeat_at: new Date().toISOString(),
-      }),
-    });
-
-    console.log("[heartbeat] response", res.status, await res.text());
-  } catch (err) {
-    console.error("[heartbeat] ERROR", err);
-  }
+      discord_stats: {
+        guild_count: client.guilds.cache.size,
+        total_users: client.guilds.cache.reduce((acc, g) => acc + g.memberCount, 0),
+        cached_users: client.users.cache.size,
+        channels_count: client.channels.cache.size,
+      },
+      guilds: client.guilds.cache.map((g) => ({
+        id: g.id,
+        name: g.name,
+        icon_url: g.iconURL(),
+        member_count: g.memberCount,
+        channel_count: g.channels.cache.size,
+        boost_level: g.premiumTier,
+      })),
+      last_heartbeat_at: new Date().toISOString(),
+    }),
+  });
 }
+
 
 module.exports = { sendHeartbeat };
